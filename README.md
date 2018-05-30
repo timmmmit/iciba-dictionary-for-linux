9dd77df51d6f27cca426359bd2496a3a200955e5
Austin Zhang <austinzhang@augmentum.com.cn>
2018-05-30 19:09:47 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iciba-dictionary-for-linux
-基于Nodejs的爱词霸Linux命令行工具
+中英互译：
+每日一句（带翻译）： http://open.iciba.com/dsapi/?date=2013-05-03
+基于所查单词进行测试，可自定义测试单词数（显示得分，连续3次答对某一个单词则从单词本中删除此单词）：
+
+
+-t(test) 10 测试10个单词
+word -e(with examples) -n (no add to words book)
+-s(sentence)
+
+
---
+++ dictionary.js
@@ -0,0 +1,94 @@
+var args = process.argv.splice(2);
+route( args )
+
+
+function route( args ) {
+  //没有参数的情况下默认不显示例句，将单词加入单词本
+  var withExamples = false;
+  var addToWordsBook = true;
+
+  //如果不加参数，或者第一个参数为-s：随机每日一句
+  if ( args.length == 0 || args[1] == "-s" ) {
+    getARandomSentence();
+    return;
+  }
+
+  //如果第一个参数是-t： 单词测试
+  if ( args[1] == '-t' ) {
+
+    return;
+  }
+
+  //查单词
+  var wordOrSentence = "";
+  for ( var arg in args ) {
+    //匹配一个单词或者句子
+    var reg = /^(\w|\s)*$/;
+    if ( reg.test(arg) ) {
+      //取第一个单词或者句子
+      wordOrSentence = arg;
+      break;
+    }
+  }
+
+  if ( wordOrSentence.trim() == "" ){
+    console.log("bad request");
+    return;
+  }
+
+  if ( args.indexOf('-e') != -1 ) withExamples = !withExamples;
+  if ( args.indexOf('-n') != -1 ) addToWordsBook = !addToWordsBook;
+
+  dictionary( word, withExamples, addToWordsBook );
+}
+
+function dictionary() {
+  const rp = require('request-promise');
+  const cheerio = require('cheerio');
+
+  const options = {
+    //uri: 'http://www.iciba.com/dictionary',
+    uri: 'http://open.iciba.com/dsapi/?date=2013-05-03',
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  //Send the request and handle the response
+  rp(options)
+    .then(function ( $ ) {
+        //console.log($("h1[class=keyword]").text());
+        console.log($.text());
+    })
+    .catch(function (err) {
+        console.log( 'error ->' + err);
+    });
+}
+
+function getARandomSentence() {
+  //根据爱词霸的api，每日一句会根据所给的日期生成，这里随机生成一个在2012-1-1到2017-12-28的日期
+  var year = 2011 + Math.ceil(Math.random()*6);
+  var month = Math.ceil(Math.random(12));
+  //偷个懒，不考虑月份日期只取1-28号
+  var day = Math.ceil(Math.random(28));
+  var uri = "http://open.iciba.com/dsapi/?date=" + year + "-" + month + "-" + day;
+
+  const rp = require('request-promise');
+  const cheerio = require('cheerio');
+
+  const options = {
+    uri: uri,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  //发送请求，处理结果
+  rp(options)
+    .then(function ( $ ) {
+        console.log($.text());
+    })
+    .catch(function (err) {
+        console.log( err );
+    });
+}
