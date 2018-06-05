a96b7574326cee3683f8bbe96ae78e69b984df06
timmmmit <15935112185@163.com>
2018-06-05 17:54:59 +0800

    notebook

--- dictionary.js
+++ dictionary.js
@@ -1,12 +1,43 @@
+/**
+        Colors
+  Reset = "\x1b[0m"
+  Bright = "\x1b[1m"
+  Dim = "\x1b[2m"
+  Underscore = "\x1b[4m"
+  Blink = "\x1b[5m"
+  Reverse = "\x1b[7m"
+  Hidden = "\x1b[8m"
+
+  FgBlack = "\x1b[30m"
+  FgRed = "\x1b[31m"
+  FgGreen = "\x1b[32m"
+  FgYellow = "\x1b[33m"
+  FgBlue = "\x1b[34m"
+  FgMagenta = "\x1b[35m"
+  FgCyan = "\x1b[36m"
+  FgWhite = "\x1b[37m"
+ */
+
 var args = process.argv.splice(2);
-var wordsBookPath = '';
-route( args )
+var noteBookPath = 'notebook';
+var maxExampleLength = 3;
 
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
-function route( args ) {
+var errorColor = "\x1b[31m";
+var keyWordColor = "\x1b[32m";
+var soundMarksColor = "\x1b[33m";
+var translationColor = "\x1b[34m";
+var enSentenceColor = "\x1b[36m";
+var chSentenceColor = "\x1b[37m";
+
+index( args )
+
+function index( args ) {
   //没有参数的情况下默认不显示例句，将单词加入单词本
   var withExamples = false;
-  var addToWordsBook = true;
+  var addToNoteBook = true;
   var englishToChinese = true;
 
   //如果不加参数，或者第一个参数为-s：随机每日一句
@@ -23,7 +54,6 @@ function route( args ) {
 
   //查单词
   var wordOrSentence = "";
-  var engReg = /^(\w|\s)*$/;
   var chReg = /^[\u4e00-\u9fa5]{1,}$/;
 
   //找到要查的那个单词或者句子
@@ -34,9 +64,6 @@ function route( args ) {
     if ( !begin && params.indexOf( args[index]) == -1 ) {
       begin = true;
       wordOrSentence += args[index] + " ";
-      if ( chReg.test( args[index] ) )  {
-        englishToChinese = false;
-      }
       if ( args.length == index + 1 || params.indexOf(args[index + 1]) != -1 ) {
         break;
       }
@@ -45,9 +72,6 @@ function route( args ) {
       continue;
     }
 
-    if ( chReg.test( args[index] ) ) {
-      englishToChinese = false;
-    }
     wordOrSentence += args[index] + " ";
 
     if ( args.length == index + 1 || params.indexOf(args[index + 1]) != -1 ) {
@@ -56,26 +80,25 @@ function route( args ) {
   }
 
   wordOrSentence = wordOrSentence.trim();
+  if ( chReg.test( args[index] ) )  englishToChinese = false;
   if ( wordOrSentence == "" ){
     console.log("show how to use it");
     return;
   }
 
   if ( args.indexOf('-e') != -1 ) withExamples = !withExamples;
-  if ( args.indexOf('-n') != -1 ) addToWordsBook = !addToWordsBook;
+  if ( args.indexOf('-n') != -1 ) addToNoteBook = !addToNoteBook;
 
   if ( englishToChinese && wordOrSentence.indexOf(" ") == -1 || !englishToChinese && wordOrSentence.length <= 2 ) {
-    icibaDictionary( wordOrSentence, withExamples, addToWordsBook, englishToChinese );
+    icibaDictionary( wordOrSentence, withExamples, addToNoteBook, englishToChinese );
   } else {
-    baiduDictionary( wordOrSentence, withExamples, addToWordsBook, englishToChinese );
+    baiduDictionary( wordOrSentence, withExamples, addToNoteBook, englishToChinese );
   }
+
 }
 
-function baiduDictionary( wordOrSentence, withExamples, addToWordsBook, englishToChinese ) {
-  const rp = require('request-promise');
-  const cheerio = require('cheerio');
+function baiduDictionary( wordOrSentence, withExamples, addToNoteBook, englishToChinese ) {
   var MD5 = require('./md5.js');
-
   var appid = '20180601000170262';
   var salt = '123456';
   var seckey = 'BL9f7_JUncqXRgGf5AU2';
@@ -91,14 +114,19 @@ function baiduDictionary( wordOrSentence, withExamples, addToWordsBook, englishT
   };
 
   //Send the request and handle the response
-  rp(options)
+  var ret = { a: "a"};
+  ret = rp(options)
     .then(function ( $ ) {
       eval("var obj = " + $.text());
-      console.log(obj.trans_result[0].dst);
 
+      if ( addToNoteBook ) {
+        writeToNoteBook( wordOrSentence, obj.trans_result[0].dst );
+      }
+
+      console.log( translationColor ,obj.trans_result[0].dst.trim() );
     })
     .catch(function (err) {
-        console.log( 'error ->' + err);
+        console.log( errorColor, 'error ->' + err);
     });
 }
 
@@ -109,9 +137,6 @@ function getARandomSentence() {
   //偷个懒，不考虑月份日期只取1-28号
   var day = Math.ceil(Math.random()*28);
   var uri = "http://open.iciba.com/dsapi/?date=" + year + "-" + month + "-" + day;
-
-  const rp = require('request-promise');
-  const cheerio = require('cheerio');
 
   const options = {
     uri: uri,
@@ -125,17 +150,14 @@ function getARandomSentence() {
     .then(function ( $ ) {
       eval("var obj = " + $.text());
       var english = obj.content;
-      console.log(obj.content + "\n" + obj.note);
+      console.log( enSentenceColor, obj.content + "\n", chSentenceColor, obj.note);
     })
     .catch(function (err) {
-        console.log( err );
+        console.log( errorColor, err );
     });
 }
 
-function icibaDictionary( wordOrSentence, withExamples, addToWordsBook, englishToChinese ) {
-  const rp = require('request-promise');
-  const cheerio = require('cheerio');
-
+function icibaDictionary( wordOrSentence, withExamples, addToNoteBook, englishToChinese ) {
   const options = {
     uri: encodeURI('http://www.iciba.com/' + wordOrSentence),
     transform: function (body) {
@@ -147,12 +169,13 @@ function icibaDictionary( wordOrSentence, withExamples, addToWordsBook, englishT
   rp(options)
     .then(function ( $ ) {
       //英译中
+      var translation = '';
+      var soundMark = '';
+      var examples = "";
+
       if ( englishToChinese ) {
-        //最多展示3条例句
-        var maxExampleLength = 3;
-        var soundMark = $("div.base-speak").text().trim().replace(/\s{2,}/, '     ').replace(/\n{1,}/, '');
-        var translation = $("ul.base-list.switch_part").text().replace(/\.(\n)+/g, '\.').replace(/\s{2,}/g, ' ').trim().replace(/\s(\w{1,5})\./g, '\n$1\.')
-        var examples = "";
+        soundMark = $("div.base-speak").text().replace(/\s{2,}/g, ' ').replace(/\n{1,}/g, '').trim();
+        translation = $("ul.base-list.switch_part").text().replace(/\.(\n)+/g, '\.').replace(/\s{2,}/g, ' ').trim().replace(/\s(\w{1,5})\./g, '\n$1\.')
   
         if ( withExamples ) {
           $("div.sentence-item").each(function(index, element) {
@@ -162,16 +185,49 @@ function icibaDictionary( wordOrSentence, withExamples, addToWordsBook, englishT
 
           examples = examples.replace(/\.{3}/g, '\.').replace(/\n{1,}/, '').replace(/\s{2,}/g, ' ').replace(/(\d)\./g, '\n$1\.');
         }
-  
-        console.log(wordOrSentence + "\n" + soundMark + "\n" + translation + "\n" + examples);
 
       //中译英
       } else {
-        var translation = $("ul.base-list.switch_part").text().replace(/\s*释义\s*/, '').replace(/\n{1,}/, '').replace(/\s{2,}/g, ' ');
-        console.log( translation );
+        translation = $("ul.base-list.switch_part").text().replace(/\s*释义\s*/, '').replace(/\n{1,}/, '').replace(/\s{2,}/g, ' ');
+      }
+      console.log( wordOrSentence + "\n" + soundMark + "\n" + translation + "\n");
+      if ( translation != "" ) {
+        console.log( keyWordColor, wordOrSentence + "\n", soundMarksColor, soundMark + "\n", translationColor, translation + "\n");
+      } else {
+        addToNoteBook = false;
+        console.log('No such word');
+      }
+
+      if ( addToNoteBook ) {
+        writeToNoteBook( wordOrSentence, translation );
       }
     })
     .catch(function (err) {
         console.log( 'error ->' + err);
     });
+}
+
+function writeToNoteBook( originalText, translatedText ) {
+  //如果文件不存在则创建文件
+  var fs = require('fs');
+  fs.exists( noteBookPath, function ( exists ) {
+    if ( !exists ) {
+      fs.writeFile(noteBookPath, '', function( err ) {
+        if ( err ) console.log( err );
+      });
+    }
+  });
+
+  //如果此单词已经在生词本中则不再添加
+  var isOriginalTextExist = true;
+  var data = fs.readFileSync(noteBookPath, 'utf8');
+  if ( data.indexOf( originalText + "-->") == -1) {
+    isOriginalTextExist = false;
+  }
+
+  if ( !isOriginalTextExist ) {
+    fs.appendFile(noteBookPath, originalText + "-->" + translatedText.replace(/\n/g, '') + "\n", function (err) { 
+      if (err) console.log(err);
+    });
+  }
 }
